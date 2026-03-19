ceb99a1a51a79113a5fe55a1ee15d142797fe6cf
pawelzysnarski <your.email@example.com>
2026-03-19 18:14:34 +0100

    Zaczęcie frontu

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import {BrowserRouter,Route,Routes} from "react-router"
 import mainPageRouter from "./routes/mainPageRouter/mainPageRouter.tsx"
-import './App.css'
+import "./App.scss"
 
 function App() {
 
   return (
     <BrowserRouter>
         <Routes>
-            <Route path="/home/*" element={mainPageRouter()}/>
+            <Route path="/*" element={mainPageRouter()}/>
         </Routes>
     </BrowserRouter>
   )
